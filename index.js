743266d2a3a3d6413395e2d22350c0517b2cca54
Charalampos Fanoulis <yo@fanoulis.dev>
2023-04-23 16:23:45 +0300

    add code order login

--- index.js
+++ index.js
@@ -1,119 +1,119 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const path = require('path')
-const fs = require('fs')
-const { chromium } = require('@playwright/test')
-const bodyParser = require('body-parser')
-const ejs = require('ejs')
-const _ = require('lodash')
-const moment = require('moment')
-const axios = require('axios');
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+const { chromium } = require("@playwright/test");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const moment = require("moment");
+const axios = require("axios");
 
 // Cache agreement template
-const agreement = fs.readFileSync(`${__dirname}/views/agreement.ejs`, 'utf8')
+const agreement = fs.readFileSync(`${__dirname}/views/agreement.ejs`, "utf8");
 
 // Cache email subjects and content
-const emailContentInternal = ejs.compile(fs.readFileSync(`${__dirname}/emails/internal.ejs`, 'utf8'))
-const emailContentSignee = ejs.compile(fs.readFileSync(`${__dirname}/emails/signee.ejs`, 'utf8'))
-const signeeSubject = ejs.compile(process.env.SIGNEE_EMAIL_SUBJECT || '')
-const internalSubject = ejs.compile(process.env.INTERNAL_EMAIL_SUBJECT || '')
-const examples = JSON.parse(fs.readFileSync(`${__dirname}/examples.json`, 'utf8'))
+const emailContentInternal = ejs.compile(
+  fs.readFileSync(`${__dirname}/emails/internal.ejs`, "utf8")
+);
+const emailContentSignee = ejs.compile(
+  fs.readFileSync(`${__dirname}/emails/signee.ejs`, "utf8")
+);
+const signeeSubject = ejs.compile(process.env.SIGNEE_EMAIL_SUBJECT || "");
+const internalSubject = ejs.compile(process.env.INTERNAL_EMAIL_SUBJECT || "");
+const examples = JSON.parse(
+  fs.readFileSync(`${__dirname}/examples.json`, "utf8")
+);
 
-validateConfig()
+// validateConfig();
 
-const postmark = require('postmark')
-const client = new postmark.Client(process.env.POSTMARK_SERVER_TOKEN)
+const postmark = require("postmark");
+const client = new postmark.Client(
+  process.env.POSTMARK_SERVER_TOKEN ?? "blahblahblah"
+);
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 const viewData = {
-  title: process.env.TITLE || '',
-  exampleData: examples
-}
+  title: process.env.TITLE || "",
+  exampleData: examples,
+};
 
+/**
+ * Order code login
+ */
+app.get("/login", (req, res) => {
+  res.render("entry", viewData);
+});
+
+/**
+ * Order code login
+ */
+app.get("/sent", (req, res) => {
+  res.render("entry", viewData);
+});
 
 /**
  * Index express route
  */
-app.get('/', (req, res) => {
-  res.render('index', viewData)
-})
+app.get("/", (req, res) => {
+  res.render("index", viewData);
+});
 
+/**
+ * debug route
+ */
+
+app.get("/render/:view", (req, res) => {
+  res.render(req.params.view, viewData);
+});
 
 /**
  * Sign document express route
  */
-app.post('/sign', async (req, res) => {
-  const template = ejs.compile(agreement)
-  req.body.date = moment().format('MMMM Do, YYYY')
+app.post("/sign", async (req, res) => {
+  const template = ejs.compile(agreement);
+  req.body.date = moment().format("MMMM Do, YYYY");
 
-  const pdfAgreement = await createDocument(template(req.body))
-    req.body.agreement = pdfAgreement
-    res.render('success', _.merge(viewData, req.body))
+  const pdfAgreement = await createDocument(template(req.body));
+  req.body.agreement = pdfAgreement;
+  res.render("success", _.merge(viewData, req.body));
 
-    sendEmails(req.body)
-  
-    const fmtBody = {
-        participant: {
-            name: req.body.name,
-            email: req.body.email,
-            phone: req.body.phone,
-            dob: req.body.dob,
-            initials: req.body.initials,
-            signature: req.body.signature
-        },
-        parent: {
-            name: req.body.parentName,
-            email: req.body.parentEmail,
-            phone: req.body.parentPhone, 
-            signature: req.body.parentSignature,
-        },
-        address: {
-            street: req.body.address,
-            city: req.body.city,
-            state: req.body.state,
-            zip: req.body.zip
-        },
-        date: req.body.date,
-        agreement: req.body.agreement
-    }
-    
-    await axios.post("https://high-weasel-42.deno.dev", fmtBody)
-})
+  sendEmails(req.body);
 
+  //todo: send waiver details to pretix
+});
 
 /**
  * Generate example agreement
  */
-app.get('/example.pdf', async (req, res) => {
-  const template = ejs.compile(agreement)
-  const data = viewData.exampleData
-  data.date = moment().format('MMMM Do, YYYY')
+app.get("/example.pdf", async (req, res) => {
+  const template = ejs.compile(agreement);
+  const data = viewData.exampleData;
+  data.date = moment().format("MMMM Do, YYYY");
 
-  const pdf = await createDocument(template(data)) 
+  const pdf = await createDocument(template(data));
   res.contentType("application/pdf");
-  res.end(pdf, 'base64');
+  res.end(pdf, "base64");
+});
 
-})
+app.get("/example.html", async (req, res) => {
+  const template = ejs.compile(agreement);
+  const data = viewData.exampleData;
+  data.date = moment().format("MMMM Do, YYYY");
 
-app.get('/example.html', async (req, res) => {
-  const template = ejs.compile(agreement)
-  const data = viewData.exampleData
-  data.date = moment().format('MMMM Do, YYYY')
-
-  res.end(template(data))
-
-})
-
+  res.end(template(data));
+});
 
 /**
  * Start express server
  */
-app.listen(process.env.PORT || 3000, () => console.log('PactMaker is up and running!'))
-
+app.listen(process.env.PORT || 3000, () =>
+  console.log("PactMaker is up and running!")
+);
 
 /**
  * Send emails to the signee and internal team
@@ -121,53 +121,57 @@ app.listen(process.env.PORT || 3000, () => console.log('PactMaker is up and runn
  */
 function sendEmails(data) {
   const attachment = {
-    'Content': data.agreement,
-    'Name': `Hack Austin Liability Waiver_${data.date}.pdf`,
-    'ContentType': 'application/pdf'
-  }
+    Content: data.agreement,
+    Name: `Hack The Thames Liability Waiver_${data.date}.pdf`,
+    ContentType: "application/pdf",
+  };
 
   // Send email to customer
-  client.sendEmail({
-    'From': process.env.POSTMARK_FROM_ADDRESS,
-    'To': `${data.email}, ${data.parentEmail}`,
-    'Subject': signeeSubject(data),
-    'HtmlBody': emailContentSignee(data),
-    'Attachments': [attachment]
-  }, (err, results) => {
-    if (err) {
-      console.error(err)
-      return
+  client.sendEmail(
+    {
+      From: process.env.POSTMARK_FROM_ADDRESS,
+      To: `${data.email}, ${data.parentEmail}`,
+      Subject: signeeSubject(data),
+      HtmlBody: emailContentSignee(data),
+      Attachments: [attachment],
+    },
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      console.log("Email sent:");
+      console.log(results);
     }
-
-    console.log('Email sent:')
-    console.log(results)
-  })
-
+  );
 
   // Send email notification to internal team
   if (process.env.INTERNAL_EMAIL_RECIPIENTS) {
-    const internalRecipients = process.env.INTERNAL_EMAIL_RECIPIENTS.split(',')
+    const internalRecipients = process.env.INTERNAL_EMAIL_RECIPIENTS.split(",");
 
     internalRecipients.forEach((email) => {
-      client.sendEmail({
-        'From': process.env.POSTMARK_FROM_ADDRESS,
-        'To': email,
-        'Subject': internalSubject(data),
-        'HtmlBody': emailContentInternal(data),
-        'Attachments': [attachment]
-      }, (err, results) => {
-        if (err) {
-          console.error(err)
-          return
-        }
+      client.sendEmail(
+        {
+          From: process.env.POSTMARK_FROM_ADDRESS,
+          To: email,
+          Subject: internalSubject(data),
+          HtmlBody: emailContentInternal(data),
+          Attachments: [attachment],
+        },
+        (err, results) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
 
-        console.log('Email sent:')
-        console.log(results)
-      })
-    })
+          console.log("Email sent:");
+          console.log(results);
+        }
+      );
+    });
   }
 }
-
 
 /**
  * Create PDF document
@@ -175,18 +179,18 @@ function sendEmails(data) {
  * @param  {Function} callback Callback containing the encoded PDF buffer
  */
 async function createDocument(content, callback) {
-
-  let dataUrl = "data:text/html;base64," + btoa(unescape(encodeURIComponent(content)))
+  let dataUrl =
+    "data:text/html;base64," + btoa(unescape(encodeURIComponent(content)));
 
   const browser = await chromium.launch({
-    headless: true
+    headless: true,
   });
   const context = await browser.newContext();
-  
+
   // Create a page.
   const page = await context.newPage();
 
-  await page.goto(dataUrl)
+  await page.goto(dataUrl);
 
   const pdf = await page.pdf({
     format: "letter",
@@ -195,27 +199,21 @@ async function createDocument(content, callback) {
       top: "48px",
       bottom: "48px",
       left: "48px",
-      right: "48px"
-    }
-  })
+      right: "48px",
+    },
+  });
 
-
-  return pdf.toString('base64');
+  return pdf.toString("base64");
 }
-
-
-
-
-
 
 /**
  * Validate heroku config
  */
 function validateConfig() {
   if (!process.env.POSTMARK_FROM_ADDRESS) {
-    throw Error('No From address specified in config')
+    throw Error("No From address specified in config");
   }
   if (!process.env.POSTMARK_SERVER_TOKEN) {
-    throw Error('No Postmark server token specified in config')
+    throw Error("No Postmark server token specified in config");
   }
 }
--- public/assets/form.js
+++ public/assets/form.js
@@ -1,48 +1,63 @@
-var submit = document.querySelector('.js-submit');
-submit.disabled = true
+const submit = document.querySelector(".js-submit");
+submit.disabled = true;
 
 // Create Signature Canvas
-var signatureCanvases = document.querySelectorAll('.js-sig-canvas');
+var signatureCanvases = document.querySelectorAll(".js-sig-canvas");
 let statusArray = [];
 
-for(let i = 0; i < signatureCanvases.length; i++) {
+for (let i = 0; i < signatureCanvases.length; i++) {
   const signatureCanvas = signatureCanvases[i];
 
-  statusArray.push(true)
+  statusArray.push(true);
 
   let signaturePad = new SignaturePad(signatureCanvas, {
-    maxWidth: 2.25
+    maxWidth: 2.25,
   });
-  
-  signaturePad.addEventListener('beginStroke', function() {
-      document.querySelector('.js-sig-reset[for=' + signatureCanvas.getAttribute("for")  + ']').classList.remove('hidden');
-      statusArray[i] = false;
-      console.log(signatureCanvases)
-      console.log(statusArray)
-      console.log(signatureCanvas)
-  })
-  signaturePad.addEventListener('endStroke', function() {
-      document.querySelector('.js-sig[name=' + signatureCanvas.getAttribute("for") + ']').value = signaturePad.toDataURL();
-      if(statusArray.every((val) => val == false ) && statusArray.length == signatureCanvases.length) {
-        submit.disabled = false;
-      }
-  })
 
-  // Reset Signature button
-  document.querySelector('.js-sig-reset[for=' + signatureCanvas.getAttribute("for") + ']').addEventListener('click', function(event) {
-    event.preventDefault();
-    signaturePad.clear();
-    this.classList.add('hidden');
-    statusArray[i] = true;
-    submit.disabled = true
-
-    if(statusArray.every((val) => val == false ) && statusArray.length == signatureCanvases.length) {
+  signaturePad.addEventListener("beginStroke", function () {
+    document
+      .querySelector(
+        ".js-sig-reset[for=" + signatureCanvas.getAttribute("for") + "]"
+      )
+      .classList.remove("hidden");
+    statusArray[i] = false;
+    console.log(signatureCanvases);
+    console.log(statusArray);
+    console.log(signatureCanvas);
+  });
+  signaturePad.addEventListener("endStroke", function () {
+    document.querySelector(
+      ".js-sig[name=" + signatureCanvas.getAttribute("for") + "]"
+    ).value = signaturePad.toDataURL();
+    if (
+      statusArray.every((val) => val == false) &&
+      statusArray.length == signatureCanvases.length
+    ) {
       submit.disabled = false;
     }
+  });
 
-    console.log(signatureCanvases)
-    console.log(statusArray)
-    console.log(signatureCanvas)
-  })
+  // Reset Signature button
+  document
+    .querySelector(
+      ".js-sig-reset[for=" + signatureCanvas.getAttribute("for") + "]"
+    )
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      signaturePad.clear();
+      this.classList.add("hidden");
+      statusArray[i] = true;
+      submit.disabled = true;
 
+      if (
+        statusArray.every((val) => val == false) &&
+        statusArray.length == signatureCanvases.length
+      ) {
+        submit.disabled = false;
+      }
+
+      console.log(signatureCanvases);
+      console.log(statusArray);
+      console.log(signatureCanvas);
+    });
 }
